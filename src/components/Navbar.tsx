d8c2640ca2ee620ed93acb2f0da8d1a0130baa0d
Cameron Tredoux <camtredoux@gmail.com>
2022-07-31 00:07:48 -0600

    Framer motion to stagger animations, changed layout for smaller screen sizes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,50 @@
+import { motion } from "framer-motion";
 import Logo from "./Logo";
+
+const links = [
+  {
+    href: "/login",
+    text: "Login",
+  },
+  {
+    href: "/api",
+    text: "API",
+  },
+];
 
 const Navbar: React.FC = () => {
   return (
-    <div className="flex flex-col p-2 rounded-md w-64 text-gray-600">
-      <div className="flex items-center mb-4">
+    <div className="flex sm:flex-col justify-between sm:justify-start p-2 rounded-md sm:w-64 text-gray-600">
+      <div className="flex items-center mb-8">
         <Logo width={40} />
         <h1 className="ml-2 font-bold uppercase">Vex Collective</h1>
       </div>
-      <ul className="flex flex-col gap-4">
-        <li>Nested Layouts</li>
-      </ul>
+      <motion.ul layoutId="list" className="flex sm:flex-col gap-2">
+        {links.map((link, index) => (
+          <Link key={index} href={link.href} index={index}>
+            {link.text}
+          </Link>
+        ))}
+      </motion.ul>
     </div>
+  );
+};
+
+interface LinkProps {
+  href: string;
+  children: React.ReactNode;
+  index: number;
+}
+
+const Link: React.FC<LinkProps> = ({ href, children, index }) => {
+  return (
+    <motion.li
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: index * 0.3, duration: 1 }}
+    >
+      <a href={href}>{children}</a>
+    </motion.li>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
@@ -12,15 +13,24 @@ const Home: NextPage = () => {
           content="Visualize statistics for your Destiny 2 characters"
         />
       </Head>
-      <div className="flex flex-col sm:flex-row m-8">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="flex flex-col sm:flex-row m-8"
+      >
         <Navbar />
         <div className="bg-black w-full rounded-md p-8 flex-1">
           Example stuff Example stuff Example stuff Example stuff Example stuff
           Example stuff Example stuff Example stuff Example stuff Example stuff
           Example stuff Example stuff Example stuff Example stuff Example stuff
-          Example stuff
+          Example stuff Example stuff Example stuff Example stuff Example stuff
+          Example stuff Example stuff Example stuff Example stuff Example stuff
+          Example stuff Example stuff Example stuff Example stuff Example stuff
+          Example stuff Example stuff Example stuff Example stuff Example stuff
+          Example stuff Example stuff Example stuff Example stuff Example stuff
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
