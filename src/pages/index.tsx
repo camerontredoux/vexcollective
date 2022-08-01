84e2e0b3bf3169cbffa7c45fffe07b28812446a1
Cameron Tredoux <camtredoux@gmail.com>
2022-07-31 23:35:22 -0600

    Features section as a placeholder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,29 +20,43 @@ const Home: NextPage = () => {
         className="flex flex-col sm:flex-row m-8"
       >
         <Navbar />
-        <div className="bg-black w-full rounded-md p-8 flex-1 mt-6 sm:mt-0">
-          <h1 className="text-4xl font-bold">The Vex Collective Mind...</h1>
-          <p className="text-xl mt-4">...has some information for you...</p>
-          <br />
-          <p className="text-gray-400 mt-4">
-            Hello there! Welcome to the Vex Collective. Please login to access
-            private information about your Destiny 2 characters. Otherwise, type
-            your gamertag in the search bar to view public information.
-          </p>
-          <br />
-          <form className="mt-8 flex justify-between">
-            <input
-              className="placeholder:text-xs xs:placeholder:text-base rounded-l-md focus:outline-0 p-2 w-full bg-zinc-800 placeholder:text-zinc-600 hover:bg-zinc-900"
-              type="text"
-              placeholder="Cameron#0370"
-            />
-            <button
-              className="text-xs xs:text-base p-2 bg-sky-700 hover:bg-sky-600 active:bg-sky-700 rounded-r-md"
-              type="submit"
-            >
-              Search
-            </button>
-          </form>
+        <div className="flex flex-1 flex-col gap-4">
+          <div className="bg-black w-full rounded-md p-8 mt-6 sm:mt-0">
+            <h1 className="text-4xl font-bold">The Vex Collective Mind...</h1>
+            <p className="text-xl mt-1">...has some information for you...</p>
+            <br />
+            <p className="text-gray-400 mt-4">
+              Hello there! Welcome to the Vex Collective. Please login to access
+              private information about your Destiny 2 characters. Otherwise,
+              type your gamertag in the search bar to view public information.
+            </p>
+            <br />
+            <form className="mt-8 flex justify-between">
+              <input
+                className="placeholder:text-base xs:placeholder:text-base rounded-l-md focus:outline-0 p-2 w-full bg-zinc-800 placeholder:text-zinc-600 hover:bg-zinc-900"
+                type="text"
+                placeholder="Cameron#0370"
+              />
+              <button
+                className="text-xs xs:text-base p-2 bg-sky-700 hover:bg-sky-600 active:bg-sky-700 rounded-r-md"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+          <div className="bg-black w-full rounded-md p-8">
+            <h1 className="text-4xl font-bold">Features</h1>
+            <p className="text-xl mt-1">Tools at your disposal</p>
+            <br />
+            <div className="flex gap-2 flex-wrap">
+              <p className="feature-item">Radar Charts</p>
+              <p className="feature-item">Activity Heatmaps</p>
+              <p className="feature-item">Kill Graphs</p>
+              <p className="feature-item">Inventory Viewer</p>
+              <p className="feature-item">Agression Analysis</p>
+            </div>
+          </div>
         </div>
       </motion.div>
     </>
